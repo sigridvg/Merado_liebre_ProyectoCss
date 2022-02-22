bcba46a6c17241deca062bcd0b48f65e61ae0411
sigridvg <sigridvg96@gmail.com>
2022-02-22 20:34:49 -0300

    modifica tamañao icon -footer

--- app.js
+++ app.js
@@ -2,8 +2,10 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-
 const publicFolder = path.resolve(__dirname, './public')
+
+
+
 app.use(express.static(publicFolder))
 
 app.listen(process.env.PORT || 3000, () => {
